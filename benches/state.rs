1d68a5405986287475fd49a7793c2edd0ea41d0b
Héctor Ramón Jiménez <hector@hecrj.dev>
2025-10-31 14:03:21 +0100

    Update `wgpu` to `27`

--- benches/state.rs
+++ benches/state.rs
@@ -14,15 +14,13 @@ impl State {
         ))
         .unwrap();
 
-        let (device, queue) = block_on(adapter.request_device(
-            &wgpu::DeviceDescriptor {
-                label: Some("Benchmark Device"),
-                required_features: adapter.features(),
-                required_limits: adapter.limits(),
-                memory_hints: wgpu::MemoryHints::Performance,
-            },
-            None,
-        ))
+        let (device, queue) = block_on(adapter.request_device(&wgpu::DeviceDescriptor {
+            label: Some("Benchmark Device"),
+            required_features: adapter.features(),
+            required_limits: adapter.limits(),
+            memory_hints: wgpu::MemoryHints::Performance,
+            ..wgpu::DeviceDescriptor::default()
+        }))
         .unwrap();
 
         Self { device, queue }
